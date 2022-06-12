3e1bd0eed4545e817a37d88ad7e9976b8dd0671d
lfabl <met2net3@gmail.com>
2022-06-12 20:59:42 +0300

    Bugfix: Backup problem fixed.

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,5 +2,5 @@
 
 export PATH="$PATH:/usr/local/bin"
 dt=`date '+%Y_%m_%d_%H_%M_%S'`
-/usr/local/bin/rethinkdb-dump -c rethinkdb:28015 -f /core/backups/"core_$dt.dump"
+/usr/local/bin/rethinkdb-dump -c rethinkdb:28015 -f /backups/"core_$dt.dump"
 exit;
